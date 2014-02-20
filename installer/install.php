c1bae6daa942398bd3141f7f4a23c2df97349a7b
ThePaavero <nospam@astudios.org>
2014-02-20 23:22:19 +0200

    Added PHP installer

---
+++ installer/install.php
@@ -0,0 +1,45 @@
+<?php
+
+require 'quickcli.php';
+
+$cli = new QuickCLI\QuickCLI('Front Template Installer');
+
+$cli->line('Welcome to ' . $cli->getAppName(), 2, 'light_cyan');
+
+$options = array(
+		'flag_a' => $cli->getFlag('a')
+	);
+
+if($options['flag_a'] === true)
+{
+	$cli->line('-a was used', 1, 'gray');
+}
+
+$project_name = $cli->prompt('Enter name of project (computer format, lowercase)', true);
+$default_project_name = 'project';
+
+$replace = array($default_project_name, ucfirst($default_project_name));
+$with    = array($project_name, ucfirst($project_name));
+
+$basedir = __DIR__ . '/../';
+
+// Rename directories
+rename($basedir . 'assets/src/js/Project', $basedir . 'assets/src/js/' . ucfirst($project_name));
+
+// Rename files
+rename($basedir . 'assets/src/scss/project.scss', $basedir . 'assets/src/scss/' . $project_name . '.scss');
+
+$file_paths = array(
+		$basedir . 'index.html',
+		$basedir . 'Gruntfile.js',
+		$basedir . 'assets/src/js/main.js',
+		$basedir . 'assets/src/js/' . ucfirst($project_name) . '/App.js',
+	);
+
+foreach($file_paths as $file)
+{
+	file_put_contents($file, str_replace($replace, $with, file_get_contents($file)));
+	$cli->line('Did "' . basename($file) . '"', 1, 'cyan');
+}
+
+$cli->line('Project data renamed from "Project" to "' . ucfirst($project_name) . '"', 2, 'green');
